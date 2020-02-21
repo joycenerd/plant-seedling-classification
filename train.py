37efca1d7650708f06b72885cf81c7d972ed2803
joycenerd <joycenerd@csie.io>
2020-02-21 12:43:23 +0800

    modified train

--- train.py
+++ train.py
@@ -13,61 +13,53 @@ from torch.autograd import Variable
 ROOTDIR="./ignore/plant-seedlings-classification"
 
 def train():
+    data_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    train_set = PlantSeedlingDataset(ROOTDIR.joinpath('train'), data_transform)
+    data_loader = DataLoader(dataset=train_set, batch_size=32, shuffle=True, num_workers=1)
 
-    # training data initialization
-    data_transform=transforms.Compose([transforms.RandomResizedCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5])])
-    
-    train_path=Path(ROOTDIR).joinpath("train")
-    train_data=PlantSeedlingData(train_path,data_transform)
-    data_loader=DataLoader(dataset=train_data,batch_size=32,shuffle=True,num_workers=1)
-
-    # parameters initialization
     device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model=VGG16(num_classes=train_data.num_classes).to(device)
+    model = VGG16(num_classes=train_set.num_classes)
+    model = model.cuda(device)
     model.train()
-    best_model_parameters=copy.deepcopy(model.state_dict())
-    best_accuracy=0.0
-    epochs=50
-    loss_function=nn.CrossEntropyLoss()
-    optimizer=torch.optim.SGD(params=model.parameters(),lr=0.001,momentum=0.9)
-    batch_size=32
 
-    for epoch in range(2):
-        show_progress="Epoch: "+str(epoch+1)+"/"+str(2)
-        print(show_progress)
-        print('-'*len(show_progress))
+    best_model_params = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    num_epochs = 50
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.001, momentum=0.9)
 
-        current_loss=0.0
-        current_acc=0
+    num_epochs=2
+    for epoch in range(num_epochs):
+        print(f'Epoch: {epoch + 1}/{num_epochs}')
+        print('-' * len(f'Epoch: {epoch + 1}/{num_epochs}'))
 
-        for i,(_input,label) in enumerate(data_loader):
-            _input=Variable(_input.cuda(device) if device=="cuda:0" else _input)
-            label=Variable(label.cuda(device) if device=="cuda:0" else label)
-            
-            # zero the parameter gradient
+        training_loss = 0.0
+        training_corrects = 0
+
+        for i, (inputs, labels) in enumerate(data_loader):
+            inputs = Variable(inputs.cuda(CUDA_DEVICES))
+            labels = Variable(labels.cuda(CUDA_DEVICES))
+
             optimizer.zero_grad()
 
-            # forward
-            outputs=model(_input)
-            _,predict_y=torch.max(outputs.data,1)
-            loss=loss_function(outputs,label)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
 
-            # backward
             loss.backward()
             optimizer.step()
 
-            # statistics
-            current_loss+=loss.item()*_input.size(0)
-            current_acc+=torch.sum(predict_y==label.data)
+            training_loss += loss.data[0] * inputs.size(0)
+            training_corrects += torch.sum(preds == labels.data)
 
-        total_loss=current_loss/len(train_data)
-        total_acc=current_acc/len(train_data)
-        print("length of training data: "+str(len(train_data)))
-        print("current loss: "+str(current_loss))
-        print("Loss: "+str(total_loss))
-        print("Accuracy: "+str(total_acc))
+        training_loss = training_loss / len(train_set)
+        training_acc = training_corrects / len(train_set)
+
+        print(f'Training loss: {training_loss:.4f}\taccuracy: {training_acc:.4f}\n'）
 
 
 
